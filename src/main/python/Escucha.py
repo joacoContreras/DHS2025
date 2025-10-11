2b07c676b5e110f49471f82f76331d2c33fb0c77
JoacoContreras <cjoaquin835@gmail.com>
2025-10-11 12:09:41 -0300

    Update Escucha.py

--- src/main/python/Escucha.py
+++ src/main/python/Escucha.py
@@ -4,70 +4,75 @@ from compiladorParser import compiladorParser
 from compiladorListener import compiladorListener
 from tablaDeSimbolos import TS, Variable
 
-class Escucha (compiladorListener):
-    indent = 1
-    declaracion = 0
-    profundidad = 0
-    numNodos = 0
-    
+class Escucha(compiladorListener):
+    def __init__(self):
+        self.indent = 1
+        self.declaracion = 0
+        self.profundidad = 0
+        self.numNodos = 0
+        self.ts = TS.getInstance()
+
     def enterPrograma(self, ctx:compiladorParser.ProgramaContext):
         print("Comienza el Parsing")
-    
+
     def exitPrograma(self, ctx:compiladorParser.ProgramaContext):
         print("Fin del Parsing")
-        
+        print(str(self.ts)) # Imprimir la tabla de símbolos al final
+
     def enterIwhile(self, ctx:compiladorParser.IwhileContext):
-        print(" "*self.indent + "Comienza while")
+        print(" " * self.indent + "Comienza while")
         self.indent += 1
-    
+
     def exitIwhile(self, ctx:compiladorParser.IwhileContext):
         self.indent -= 1
-        print(" "*self.indent + "Fin while")
-        
+        print(" " * self.indent + "Fin while")
+
+    # MÉTODO CORREGIDO
     def enterDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
-        from tablaDeSimbolos import TS, Variable
+        # Comprobación de seguridad para evitar el error
+        if ctx.getChildCount() == 0:
+            print("ADVERTENCIA: Se encontró una regla de 'declaracion' vacía.")
+            return
+
         self.declaracion += 1
         print("Declaracion Enter -> |" + ctx.getText() + "|")
         print(" -- Cant. hijos = " + str(ctx.getChildCount()))
+
         # Extraer tipo y nombre de variable
         tipo = ctx.getChild(0).getText()  # INT o DOUBLE
         nombre = ctx.getChild(1).getText()  # ID principal
-        ts = TS.getInstance()
-        var = Variable(nombre, tipo)
-        ts.addSimbolo(var)
-        print(f"   [TS] Variable agregada: {nombre} ({tipo})")
         
-    def exitDeclaration(self, ctx:compiladorParser.DeclaracionContext):
+        var = Variable(nombre, tipo)
+        self.ts.addSimbolo(var)
+        print(f"   [TS] Variable agregada: {nombre} ({tipo})")
+
+    # MÉTODO CORREGIDO
+    def exitDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
         print("Declaracion EXIT -> |" + ctx.getText() + "|")
-        print(" -- Cant. hijos = " + str(ctx.getChildCount()))
-    
+
     def enterListavar(self, ctx:compiladorParser.ListavarContext):
         self.profundidad += 1
-    
+
     def exitListavar(self, ctx:compiladorParser.ListavarContext):
         self.profundidad -= 1
-        print("     -- ListaVar(%d) Cant. hijos = %d" % (self.profundidad, ctx.getChildCount()))
-        if ctx.getChildCount() == 4 :
-            print("          hijo ID --> |%s|" % ctx.getChild(1).getText())
-        
+        # Lógica para procesar la lista de variables si es necesario
+        if ctx.getChildCount() > 1:
+            nombre = ctx.getChild(1).getText()
+            # Aquí necesitarías saber el tipo de la declaración original para agregarla a la TS
+            # print(f"   [TS] Variable de lista agregada: {nombre}")
+
     def visitErrorNode(self, node:ErrorNode):
         print(" ---> ERROR")
-    
+
     def enterEveryRule(self, ctx):
         self.numNodos += 1
-        
+
     def __str__(self):
         return "Se hicieron " + str(self.declaracion) + " declaraciones\n" + \
                "Se visitaron " + str(self.numNodos) + " nodos\n"
-               
+
     def exitEveryRule(self, ctx):
         pass
-    
+
     def visitTerminal(self, node:TerminalNode):
         pass
-    
-    def enterDeclaration(self, ctx):
-        ts = TS.getInstance()
-        # Extrae nombre y tipo de la variable del contexto
-        # variable = Variable(nombre, tipo)
-        # ts.agregarSimbolo(variable)
