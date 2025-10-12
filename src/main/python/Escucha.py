7381d9dc4cb090fe21cf4a514c766770ed33ac2e
JoacoContreras <cjoaquin835@gmail.com>
2025-10-12 17:30:10 -0300

    Update Escucha.py

--- src/main/python/Escucha.py
+++ src/main/python/Escucha.py
@@ -14,16 +14,22 @@ class Escucha (compiladorListener) :
         print("Comienza el parsing")
 
     def exitPrograma(self, ctx:compiladorParser.ProgramaContext):
-            print("Fin del Parsing")
-            from tablaDeSimbolos import TS
-            ts = TS.getInstance()
-            print(ts)
+        print("Fin del parsing")
+        ts = TS.getInstance()
+        print(ts)
 
     def enterIwhile(self, ctx:compiladorParser.IwhileContext):
         print("  "*self.indent + "Comienza while")
         self.indent += 1
 
+    def exitIwhile(self, ctx:compiladorParser.IwhileContext):
+        self.indent -= 1
+        print("  "*self.indent + "Fin while")
 
+    def enterDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
+        self.declaracion += 1
+        print("Declaracion ENTER -> |" + ctx.getText() + "|")
+        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
         tipo = None
         nombres = []
         for i in range(ctx.getChildCount()):
@@ -42,6 +48,28 @@ class Escucha (compiladorListener) :
             print(f"   [TS] Variable agregada: {nombre} ({tipo})")
         if not nombres:
             print("   [TS] Declaración vacía o incompleta, no se agrega símbolo.")
+    
+    def exitDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
+        print("Declaracion EXIT  -> |" + ctx.getText() + "|")
+        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
+    
+    def enterListavar(self, ctx:compiladorParser.ListavarContext):
+        self.profundidad += 1
+
+    def exitListavar(self, ctx:compiladorParser.ListavarContext):
+        print("  -- ListaVar(%d) Cant. hijos  = %d" % (self.profundidad, ctx.getChildCount()))
+        self.profundidad -= 1
+        if ctx.getChildCount() == 4 :
+            print("      hoja ID --> |%s|" % ctx.getChild(1).getText())
+
+    # def visitTerminal(self, node: TerminalNode):
+    #     print(" ---> Token: " + node.getText())
+        # self.numTokens += 1
+    
+    def visitErrorNode(self, node: ErrorNode):
+        print(" ---> ERROR")
+        
+    def enterEveryRule(self, ctx):
         self.numNodos += 1
     
     def __str__(self):
