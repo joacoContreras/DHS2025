26bd46ac3de2dcf89053848f6350934dc7bed7b6
JoacoContreras <cjoaquin835@gmail.com>
2025-10-12 12:14:39 -0300

    Update Escucha.py

--- src/main/python/Escucha.py
+++ src/main/python/Escucha.py
@@ -2,6 +2,7 @@ from antlr4 import TerminalNode
 from antlr4 import ErrorNode
 from compiladorParser import compiladorParser
 from compiladorListener import compiladorListener
+from tablaDeSimbolos import TS, Variable
 
 class Escucha (compiladorListener) :
     indent = 1
@@ -13,43 +14,34 @@ class Escucha (compiladorListener) :
         print("Comienza el parsing")
 
     def exitPrograma(self, ctx:compiladorParser.ProgramaContext):
-        print("Fin del parsing")
+            print("Fin del Parsing")
+            from tablaDeSimbolos import TS
+            ts = TS.getInstance()
+            print(ts)
 
     def enterIwhile(self, ctx:compiladorParser.IwhileContext):
         print("  "*self.indent + "Comienza while")
         self.indent += 1
 
 
-    def exitIwhile(self, ctx:compiladorParser.IwhileContext):
-        self.indent -= 1
-        print("  "*self.indent + "Fin while")
-
-    def enterDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
-        self.declaracion += 1
-        print("Declaracion ENTER -> |" + ctx.getText() + "|")
-        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
-    
-    def exitDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
-        print("Declaracion EXIT  -> |" + ctx.getText() + "|")
-        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
-    
-    def enterListavar(self, ctx:compiladorParser.ListavarContext):
-        self.profundidad += 1
-
-    def exitListavar(self, ctx:compiladorParser.ListavarContext):
-        print("  -- ListaVar(%d) Cant. hijos  = %d" % (self.profundidad, ctx.getChildCount()))
-        self.profundidad -= 1
-        if ctx.getChildCount() == 4 :
-            print("      hoja ID --> |%s|" % ctx.getChild(1).getText())
-
-    # def visitTerminal(self, node: TerminalNode):
-    #     print(" ---> Token: " + node.getText())
-        # self.numTokens += 1
-    
-    def visitErrorNode(self, node: ErrorNode):
-        print(" ---> ERROR")
-        
-    def enterEveryRule(self, ctx):
+        tipo = None
+        nombres = []
+        for i in range(ctx.getChildCount()):
+            hijo = ctx.getChild(i)
+            texto = hijo.getText()
+            if i == 0:
+                tipo = texto
+            if texto.isidentifier() and texto != tipo:
+                nombres.append(texto)
+        print(f"   Tipo detectado: {tipo}")
+        print(f"   Identificadores detectados: {nombres}")
+        ts = TS.getInstance()
+        for nombre in nombres:
+            var = Variable(nombre, tipo)
+            ts.addSimbolo(var)
+            print(f"   [TS] Variable agregada: {nombre} ({tipo})")
+        if not nombres:
+            print("   [TS] Declaración vacía o incompleta, no se agrega símbolo.")
         self.numNodos += 1
     
     def __str__(self):
